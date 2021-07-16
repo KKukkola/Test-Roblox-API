7493a46a5ebef9cf06b4ccf09314d36b40965e55
Kyle Kukkola <kkukkola33@gmail.com>
2021-07-15 23:21:15 -0700

    added DB.js - module for saving/loading data

---
+++ src/modules/DB.js
@@ -0,0 +1,19 @@
+// Simple module for writing/reading data for the application
+
+const electron = require('electron');
+
+const {app} = electron;
+
+//***********************************************//
+
+export function InsertID(id){
+
+}
+
+export function RemoveID(id){
+
+}
+
+export function GetIDs(){
+
+}
