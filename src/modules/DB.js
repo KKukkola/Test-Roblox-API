5331bd28b84a616f18e10c9962c4beb67f6f6f46
Kyle Kukkola <kkukkola33@gmail.com>
2021-07-16 23:57:22 -0700

    a folder for data is now created in the user's AppData.

--- src/modules/DB.js
+++ src/modules/DB.js
@@ -1,16 +1,27 @@
 // Simple module for writing/reading data for the application
 
 const electron = require('electron');
-
-const {app} = electron;
+const fs = require('fs');
+const path = require('path');
 
 var userdata;
 
 //***********************************************//
 
-exports.SetPath = function(path){
-    console.log("setting DB path to: " + path);
-    userdata  = path;
+exports.SetPath = function(dir){
+    console.log("setting DB path to: " + dir);
+
+    // Attempt to first-time setup our data
+
+    fs.mkdir(dir + '/my_data', function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('successfully created a directory: ' + dir + '/my_data');
+        }
+    })
+
+    userdata = path.join(dir, '/my_data');
 }
 
 exports.GetPath = function(){
@@ -18,7 +29,12 @@ exports.GetPath = function(){
 }
 
 exports.InsertID = function(id){
-    console.log("TODO: DB.InsertID");
+    fs.appendFile(path.join(userdata + '/user_ids.txt'), String(id) + ',', function(err) {
+        if (err) throw err;
+        console.log("Inserted an ID!");
+    });
+
+    console.log("id inserted!");
 
     return true;
 }
